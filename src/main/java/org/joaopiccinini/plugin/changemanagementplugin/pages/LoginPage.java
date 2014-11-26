73df053d1d633814d95b7b5fb2d11445a9e83856
Joao Piccinini <joaopiccinini@gmail.com>
2014-11-25 22:52:12 -0200

    Configuring the logging messages.

--- src/main/java/org/joaopiccinini/plugin/changemanagementplugin/pages/LoginPage.java
+++ src/main/java/org/joaopiccinini/plugin/changemanagementplugin/pages/LoginPage.java
@@ -5,8 +5,12 @@ import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginPage {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginPage.class);
 
 	private WebDriver driver;
 
@@ -29,8 +33,11 @@ public class LoginPage {
 	}
 
 	public void authenticate(String user, String password) {
+		LOGGER.info("Inserting the user credentials inside the web elements.");
 		cdUsuario.sendKeys(user);
 		cdSenha.sendKeys(password);
+
+		LOGGER.info("Clicking in the login button to authenticate.");
 		btnLogin.click();
 	}
 
